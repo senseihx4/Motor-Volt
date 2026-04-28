2f2ccdccb292e741489457e18b5b37c59032df47
senseihx4 <senseihx4@users.noreply.github.com>
2026-04-28 12:01:13 +0530

    Remove MEDIA_URL to fix Cloudinary

--- cars/settings.py
+++ cars/settings.py
@@ -92,8 +92,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'login.User'
 
