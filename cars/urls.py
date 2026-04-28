ebe3687430f5fb4459b88b139c9430c58100d1fd
senseihx4 <senseihx4@users.noreply.github.com>
2026-04-28 11:41:03 +0530

    Fix settings for production

--- cars/urls.py
+++ cars/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('home'), name='root'),
     path('', include('login.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
